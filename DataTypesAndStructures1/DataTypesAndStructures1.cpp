9e0cc0d5a825c0c450b7b50da31818a09bfb768c
delokoseni <artur.b3lsh@gmail.com>
2023-01-31 18:34:50 +0700

    Добавлены функции ввода и вывода вектора, функции сортировки вектора

--- DataTypesAndStructures1/DataTypesAndStructures1.cpp
+++ DataTypesAndStructures1/DataTypesAndStructures1.cpp
@@ -4,23 +4,29 @@
 #include <algorithm> 
 using namespace std;
 
-void SimpleSorting(double* arr, int size);
-void ImprovedSorting(double* arr, int left, int right);
-void Exchange(double* arr, int i, int j);
 void InputArray(double* arr, int size);
 void OutputArray(double* arr, int size);
+void SimpleSorting(double* arr, int size);
+void ImprovedSorting(double* arr, int left, int right);
 int BinarySearch(double* arr, int size, double key);
+void InputVector(vector<double> *myvector);
+void OutputVector(vector<double> *myvector);
+void VectorSimpleSorting(vector<double> *myvector);
+void VectorImprovedSorting(vector<double> *myvector, int left, int right);
+int VectorBinarySearch(vector<double> *myvector, double key);
 
 int main()
 {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
     int n;
-    double *arr, search = 1.72896;
+    double* arr, search = 1.72896;
     cout << "Введите размер массива: ";
     cin >> n;
     arr = new double[n];
-    InputArray(arr, n);
+    vector<double> myvector(n);
+    vector<double>::iterator it; // итератор вектора типа double
+    /*InputArray(arr, n);
     SimpleSorting(arr, n);
     cout << "Результат сортировки выбором: ";
     OutputArray(arr, n);
@@ -30,42 +36,54 @@ int main()
     cout << "Результат сортировки Хоара: ";
     OutputArray(arr, n);
     cout << endl;
-    cout << "Результат поиска индекса элемента равного " << search << " в массиве: " << BinarySearch(arr, n, search);
+    cout << "Результат поиска индекса элемента равного " << search << " в массиве: " << BinarySearch(arr, n, search);*/
+    InputVector(&myvector);
+    OutputVector(&myvector);
+    VectorSimpleSorting(&myvector);
+    cout << "Результат сортировки выбором: ";
+    OutputVector(&myvector);
+    InputVector(&myvector);
+    VectorImprovedSorting(&myvector, 0, n-1);
+    cout << "Результат сортировки Хоара: ";
+    OutputVector(&myvector);
+    //cout << "Результат поиска индекса элемента равного " << search << " в векторе: " << VectorBinarySearch(&myvector, search);
     return 0;
 }
 
-void SimpleSorting(double *arr, int size) 
+void SimpleSorting(double* arr, int size)
 {
     int i, j, k;
     double x;
     for (i = 0; i < size; i++) { // i - номер текущего шага
-        k = i; 
+        k = i;
         x = *(arr + i);
         for (j = i + 1; j < size; j++) // цикл выбора наименьшего элемента
             if (*(arr + j) < x) {
-                k = j; 
+                k = j;
                 x = *(arr + j); // k - индекс наименьшего элемента
             }
-        *(arr + k) = *(arr + i); 
+        *(arr + k) = *(arr + i);
         *(arr + i) = x; // меняем местами наименьший с *(arr + i)
     }
 }
 
 void ImprovedSorting(double* arr, int left, int right)
 {
-    int i, j, h;
-    double m;
+    int i, j;
+    double m, buffer;
     i = left;
     j = right;
-    m = *(arr + (i + j + 1) / 2);
+    m = *(arr + (i + j + 1) / 2); // серединный элемент
     do {
-        while (*(arr+i) < m) 
-            i++; 
+        while (*(arr + i) < m)
+            i++;
         while (*(arr + j) > m)
-            j--; 
-        if (i <= j) {
-            Exchange(arr, i, j);//обмен i, j элементов местами
-            i++; 
+            j--;
+        if (i <= j) { //обмен i, j элементов местами
+            buffer = *(arr + i);
+            *(arr + i) = *(arr + j);
+            *(arr + j) = buffer;
+            i++;
             j--;
         }
     } while (i <= j);
@@ -75,15 +93,7 @@ void ImprovedSorting(double* arr, int left, int right)
         ImprovedSorting(arr, i, right);
 }
 
-void Exchange(double* arr, int i, int j) 
-{
-    double buffer;
-    buffer = *(arr + i);
-    *(arr + i) = *(arr + j);
-    *(arr + j) = buffer;
-}
-
-void InputArray(double *arr, int size)
+void InputArray(double* arr, int size)
 {
     int i;
     for (i = 0; i < size; i++) {
@@ -105,10 +115,10 @@ int BinarySearch(double* arr, int size, double key) {
     int right = size; // правая граница массива
     int mid; // срединный индекс отрезка [left, right]
     while (left < right) {
-        mid = (left + right) / 2; 
-        if (*(arr + mid) > key) 
+        mid = (left + right) / 2;
+        if (*(arr + mid) > key)
             right = mid; // проверяем, какую часть нужно отбросить с поиска
-        else 
+        else
             left = mid + 1;
     }
     right--;
@@ -117,3 +127,71 @@ int BinarySearch(double* arr, int size, double key) {
     else
         return -1; // ключ не найден
 }
+
+void InputVector(vector<double> *myvector)
+{
+    vector<double>::iterator it; // итератор вектора типа double
+    for (it = (*myvector).begin(); it < (*myvector).end(); it++) // присвоение итератору значения итератора вектора myvector
+    {
+        cout << "Введите элемент вектора: ";
+        cin >> *it;
+    }
+}
+
+void OutputVector(vector<double> *myvector)
+{
+    vector<double>::iterator it; // итератор вектора типа double
+    for (it = (*myvector).begin(); it < (*myvector).end(); it++)
+    {
+        cout << *it << " ";
+    }
+}
+
+void VectorSimpleSorting(vector<double> *myvector)
+{
+    vector<double>::iterator it, jt, kt;
+    double x;
+    for (it = (*myvector).begin(); it < (*myvector).end(); it++) { // i - номер текущего шага
+        kt = it;
+        x = *it;
+        for (jt = it + 1; jt < (*myvector).end(); jt++) // цикл выбора наименьшего элемента
+            if (*jt < x) {
+                kt = jt;
+                x = *jt;
+            }
+        *kt = *it;
+        *it = x; // меняем местами наименьший с *(arr + i)
+    }
+}
+
+void VectorImprovedSorting(vector<double> *myvector, int left, int right)
+{
+    int i, j;
+    double m, buffer;
+    i = left;
+    j = right;
+    m = myvector->at((i + j + 1) / 2); // серединный элемент
+    do {
+        while (myvector->at(i) < m)
+            i++;
+        while (myvector->at(j) > m)
+            j--;
+        if (i <= j) { //обмен i, j элементов местами
+            buffer = myvector->at(i);
+            myvector->at(i) = myvector->at(j);
+            myvector->at(j) = buffer;
+            i++;
+            j--;
+        }
+    } while (i <= j);
+    if (left < j)
+        VectorImprovedSorting(myvector, left, j);
+    if (i < right)
+        VectorImprovedSorting(myvector, i, right);
+}
+
+int VectorBinarySearch(vector<double> *myvector, double key)
+{
+    return 0;
+}
+   
